e9a7f9c8fc9e86c7d62595b9caa6df219731bfe8
Samet Koyuncu <sametkoyuncu@live.com>
2021-09-06 20:27:03 +0300

    Install vuetify

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
+
 const app = express()
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
 app.listen(port, () => {
