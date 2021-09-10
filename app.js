87b6f7ff607b9fabe85910138fecbedc407faf70
Samet Koyuncu <sametkoyuncu@live.com>
2021-09-10 12:52:49 +0300

    Create single post page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require('express')
+const Handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
 require('./mongo-connection')
 const app = express();
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const mainRouter = require('./routes/main')
 const dashboardRouter = require('./routes/dashboard')
 const bodyParser = require('body-parser')
@@ -10,8 +12,10 @@ const port = 3000
 
 app.use(express.static('public'))
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+app.set('view engine', 'handlebars')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
