27e69a4c2fba72e58e998b692efb247af5bef09d
Samet Koyuncu <sametkoyuncu@live.com>
2021-09-18 21:41:03 +0300

    Add post update and delete for dashboard

--- routes/dashboard/post.js
+++ routes/dashboard/post.js
@@ -42,4 +42,76 @@ router.post('/new', function (req, res) {
     // res.status(201).render('dashboard/posts', { layout: 'dashboard' })
 })
 
+router.get('/edit/:id', function (req, res) {
+    Post.findOne({ _id: req.params.id })
+        .populate({ path: 'category', model: Category })
+        .then(post => {
+            Category.find({})
+                .then(categories => {
+                    res.render('dashboard/post-edit', {
+                        layout: 'dashboard',
+                        title: 'Blog Düzenle',
+                        post: post,
+                        categories: categories
+                    })
+                })
+        })
+})
+
+router.put('/edit/:id', function (req, res) {
+    if (req.body.image != null) {
+        let postImage = req.files.image
+        let postImageName = Date.now() + postImage.name
+        postImage.mv(path.resolve(__dirname, '../../public/img/uploaded/posts', postImageName))
+
+        Post.findOne({ _id: req.params.id }).then(post => {
+            post.title = req.body.title
+            post.content = req.body.content
+            post.category = req.body.category
+            post.date = req.body.date
+            post.image = `/img/uploaded/posts/${postImageName}`
+            post.author = req.session.userId
+
+            post.save().then(post => {
+                req.session.sessionFlash = {
+                    type: 'alert alert-success',
+                    message: `'${req.body.title}' isimli blog başarılı bir şekilde güncellendi.`
+                }
+
+                res.redirect('/dashboard/posts')
+            })
+        })
+    }
+
+    Post.findOne({ _id: req.params.id }).then(post => {
+        post.title = req.body.title
+        post.content = req.body.content
+        post.category = req.body.category
+        post.date = req.body.date
+        post.author = req.session.userId
+
+        post.save().then(post => {
+            req.session.sessionFlash = {
+                type: 'alert alert-success',
+                message: `'${req.body.title}' isimli blog başarılı bir şekilde güncellendi.`
+            }
+
+            res.redirect('/dashboard/posts')
+        })
+    })
+
+    
+})
+
+router.delete('/:id', function (req, res) {
+    Post.deleteOne({ _id: req.params.id }).then(() => {
+        req.session.sessionFlash = {
+            type: 'alert alert-success',
+            message: 'Blog başarılı bir şekilde silindi.'
+        }
+
+        res.redirect('/dashboard/posts')
+    })
+})
+
 module.exports = router
