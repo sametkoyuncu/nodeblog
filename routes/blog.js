e2416f06b4745f0e0f1fd60427595c4926212d11
Samet Koyuncu <sametkoyuncu@live.com>
2021-09-20 22:51:20 +0300

    Add search area for post titles

--- routes/blog.js
+++ routes/blog.js
@@ -32,31 +32,6 @@ router.get('/', function (req, res) {
         })
 })
 
-router.get('/:id', function (req, res) {
-    const postId = req.params.id
-    Post.findById({ _id: postId })
-        .populate({ path: 'author', model: User })
-        .populate({ path: 'category', model: Category })
-        .then(post => {
-            Post.find({})
-                .sort({ $natural: -1 })
-                .limit(3)
-                .populate({ path: 'category', model: Category })
-                .then(posts => {
-                    Category.find({}).then(categories => {
-                        res.render('blog-single', {
-                            post: post,
-                            title: post.title,
-                            posts: posts,
-                            categories: categories
-                        })
-                    })
-                })
-
-        })
-})
-
-
 router.get('/categories/:id', function (req, res) {
     const categoryId = req.params.id
     Post.find({ category: categoryId })
@@ -92,6 +67,71 @@ router.get('/categories/:id', function (req, res) {
 
                         })
                 })
+        })
+})
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+router.get('/search', function (req, res) {
+    if (req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        Post.find({ "title": regex })
+            .populate({ path: 'category', model: Category })
+            .sort({ $natural: -1 })
+            .then(posts => {
+                Category
+                    .aggregate([
+                        {
+                            $lookup: {
+                                from: 'posts',
+                                localField: '_id',
+                                foreignField: 'category',
+                                as: 'posts'
+                            }
+                        },
+                        {
+                            $project: {
+                                _id: 1,
+                                name: 1,
+                                num_of_posts: { $size: '$posts' }
+                            }
+                        }
+                    ])
+                    .then(categories => {
+                        res.render('blog-category', {
+                            posts: posts,
+                            title: 'Arama Sonuçları',
+                            categories: categories,
+                            pageHeader: `'${req.query.search}' için arama sonuçları: ${posts.length} eşleşme bulundu.`
+                        })
+                    })
+            })
+    }
+})
+
+router.get('/:id', function (req, res) {
+    const postId = req.params.id
+    Post.findById({ _id: postId })
+        .populate({ path: 'author', model: User })
+        .populate({ path: 'category', model: Category })
+        .then(post => {
+            Post.find({})
+                .sort({ $natural: -1 })
+                .limit(3)
+                .populate({ path: 'category', model: Category })
+                .then(posts => {
+                    Category.find({}).then(categories => {
+                        res.render('blog-single', {
+                            post: post,
+                            title: post.title,
+                            posts: posts,
+                            categories: categories
+                        })
+                    })
+                })
+
         })
 })
 
